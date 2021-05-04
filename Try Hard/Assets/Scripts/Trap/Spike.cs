a9212672fd029cf0208014bdc07d06e685f6a9bc
longbale-71 <long7hoang@gmail.com>
2021-05-04 14:50:18 +0700

    dsdf
    sdfsdf

---
+++ Try Hard/Assets/Scripts/Trap/Spike.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public float SpikeSpeed;
+    public Rigidbody2D rigidbody2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2.velocity = Vector2.down * SpikeSpeed;
+    }
+}
